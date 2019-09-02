146cf907c3a0f0b224ea876537695c6ec3d610ae
jsoo.sh2221 <jsoo.sh2221@gmail.com>
2019-09-02 18:14:50 +0900

    ssssssggg

--- SeenPro/src/main/java/com/controller/WorkController.java
+++ SeenPro/src/main/java/com/controller/WorkController.java
@@ -3,6 +3,7 @@ package com.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,6 +12,7 @@ import com.dto.WorkDTO;
 import com.service.MemberService;
 import com.service.WorkService;
 
+@Controller
 public class WorkController {
 
 	@Autowired
@@ -23,10 +25,12 @@ public class WorkController {
 	@RequestMapping("/workList")
 	public ModelAndView workList(@RequestParam("wCategory") String wCategory) {
 		
+		System.out.println(wCategory);
 		List<WorkDTO> list = wservice.workList(wCategory);
+		System.out.println(list);
 		ModelAndView mav = new ModelAndView();
 		mav.addObject("workList",list);
-		mav.setViewName("main");
+		mav.setViewName("workList");
 		return mav;
 	}
 	
